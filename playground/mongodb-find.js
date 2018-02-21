3eccaef501ca7ed2f085f611b9379dd39513ce39
Rickson Muchedzi <rrmuchedzi@gmail.com>
2018-02-21 15:32:43 +0200

    initial commit

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -1,37 +1,40 @@
-const {MongoClient, ObjectID} = require('mongodb');
+const {
+    MongoClient,
+    ObjectID
+} = require('mongodb');
 
-MongoClient.connect("mongodb://localhost:27017/TodoApp", (error, client)=> {
-    if(error) {
+MongoClient.connect("mongodb://localhost:27017/TodoApp", (error, client) => {
+    if (error) {
         return console.log("Failed to Connect : ", error);
     }
     const dbName = "TodoApp";
     console.log("MongoDB-FIND Connected successfully");
 
-    // client.db(dbName).collection("Todo").find({
-    //     _id: new ObjectID('5a8d589e69ef279d451dd2ab')
-    // }).toArray()
-    // .then( (result) => {
-    //     console.log("Document Retrieved Successfully \n", JSON.stringify(result, undefined, 2));
-    //     if( !result.completed ) {
-    //         console.log("Task not yet completed")
-    //     }
-    // }, (error) => {
-    //     console.log("Failed to retrieve the files from the database: ", error);
-    // } )
-
-    // client.db(dbName).collection("Todo").find().count()
-    // .then( (countResult) => {
-    //     console.log(`Todo List Has : ${countResult} Listed Available`);
-    // }, (error) => {
-    //     console.log("Failed to retrieve the files from the database: ", error);
+    // client.db(dbName).collection('Todo').deleteOne({
+    //     text: "Study MongoDB Management"
+    // })
+    // .then((result)=> {
+    //     console.log(result);
     // })
 
-    client.db(dbName).collection('Users').find({
-        name: 'Rickson'
-    }).toArray().then( ( queeryResult ) => {
-        console.log(JSON.stringify(queeryResult, undefined, 2));
-    }, (error) => {
-        console.log( 'An error occued : ', error);
+    // client.db(dbName).collection('Todo').findOneAndDelete({
+    //     text: 'To register for the GYM'
+    // }).then( (result) => {
+    //     console.log(result);
+    // }, (error) => {
+    //     console.log(error);
+    // });
+
+    client.db(dbName).collection('Users').findOneAndReplace({
+        _id: new ObjectID('5a8d65d769ef279d451dd434')
+    }, {
+        name: "Keanu",
+        age: 55,
+        location: "United States"
+    }).then((deleteResult) => {
+        console.log(deleteResult);
+    }, (err) => {
+        console.log("An error Occurred", err);
     });
 
     //client.close();
