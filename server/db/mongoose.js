63cc64be9a235b6caa8216aeaec7a0ca177b8b49
Rickson Muchedzi <rrmuchedzi@gmail.com>
2018-03-14 14:54:26 +0200

    Final Revision for the API CRUD and development ENV

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,5 @@
 var mongooseRef = require('mongoose');
 mongooseRef.Promise = global.Promise;
-mongooseRef.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongooseRef.connect(process.env.MONGODB_URI);
 
 module.exports = mongooseRef;
